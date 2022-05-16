1117780ebdf6a6350e158cf5e4de5fa99cbcdf47
ljhhhhhh1224 <3364879467@qq.com>
2022-05-16 13:27:11 +0800

    update ut

--- ginServer/ginserver_test.go
+++ ginServer/ginserver_test.go
@@ -1,7 +1,24 @@
 package ginServer
 
-import "testing"
+import (
+	"net/http"
+	"testing"
 
-func TestSetupRouter(t *testing.T) {
-	//r := SetupRouter()
+	"github.com/go-playground/assert/v2"
+
+	"github.com/appleboy/gofight/v2"
+)
+
+func TestGet(t *testing.T) {
+	r := gofight.New()
+	r.GET("/get").Run(SetupRouter(), func(response gofight.HTTPResponse, request gofight.HTTPRequest) {
+		assert.Equal(t, http.StatusOK, response.Code)
+	})
+}
+
+func TestStop(t *testing.T) {
+	r := gofight.New()
+	r.GET("/stop").Run(SetupRouter(), func(response gofight.HTTPResponse, request gofight.HTTPRequest) {
+		assert.Equal(t, http.StatusOK, response.Code)
+	})
 }
--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module mini_cmux
 go 1.16
 
 require (
+	github.com/appleboy/gofight/v2 v2.1.2 // indirect
 	github.com/gin-gonic/gin v1.7.7
+	github.com/go-playground/assert/v2 v2.0.1 // indirect
 	golang.org/x/net v0.0.0-20220412020605-290c469a71a5
 	google.golang.org/grpc v1.46.0
 	google.golang.org/protobuf v1.28.0
