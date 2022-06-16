908933097c90f6667314941ae076669d8afd1836
ljhhhhhh1224 <3364879467@qq.com>
2022-06-16 18:59:37 +0800

    update

--- client/client.go
+++ client/client.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 
 	hello_grpc "github.com/ljhhhhhh1224/mini_cmux/pb"
@@ -29,52 +30,52 @@ func httpGet() {
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", "http://localhost:23456/get", nil)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 	}
 	req.Header.Add("content-type", "application/json")
 	resp, err := client.Do(req)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 	}
-	fmt.Println(string(body))
+	log.Println(string(body))
 }
 
 func httpStop() {
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", "http://localhost:23456/stop", nil)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 	}
 	req.Header.Add("content-type", "application/json")
 	resp, err := client.Do(req)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 	}
-	fmt.Println(string(body))
+	log.Println(string(body))
 }
 
 func grpcRequestStop(client hello_grpc.HelloGRPCClient) {
 	req, err := client.RequestStop(context.Background(), &hello_grpc.Req{Message: "Request stop from grpc client"})
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 	}
-	fmt.Println(req.GetMessage())
+	log.Println(req.GetMessage())
 }
 
 func grpcSayHi(client hello_grpc.HelloGRPCClient) {
 	req, err := client.SayHi(context.Background(), &hello_grpc.Req{Message: "Say hi from grpc client"})
 	if err != nil {
-		fmt.Println(err)
+		log.Println(err)
 	}
-	fmt.Println(req.GetMessage())
+	log.Println(req.GetMessage())
 }
